d66ef50e2ba2573cacfedc96b734d3fae866bdc0
Rahul Kumar <rahulkaitian@gmail.com>
2015-07-28 18:29:56 +0530

    Update StringPatternMatching.java

--- StringPatternMatching.java
+++ StringPatternMatching.java
@@ -2,16 +2,11 @@ import java.util.ArrayList;
 
 
     public static void main(String[] arg) throws  {
-        String input="What is the worst-case running time of these two nested loops? The inner while\n" + 
-        "loop goes around at most m times, and potentially far less when the pattern match\n" + 
-        "fails. This, plus two other statements, lies within the outer for loop. The outer loop\n" + 
-        "goes around at most n−m times, since no complete alignment is possible once we\n" + 
-        "get too far to the right of the text. The time complexity of nested loops multiplies,\n" + 
-        "so this gives a worst-case running time of O((n − m)(m + 2)).\n" + 
-        "We did not count the time it takes to compute the length of the strings using\n" + 
-        "the function strlen. Since the implementation of strlen is not given, we must guess\n" + 
-        "how long it should take. If we explicitly count the number of characters until we";
-        String searchPattern="is";
+        matchPattern("asjfa kdfhak jdfhkjadkj kadfka ", "fa");
+    }
+    public static ArrayList matchPattern(String text, String pattern){
+         String input=text;
+        String searchPattern=pattern;
         int inptLen=input.length();
         int pattrnLen=searchPattern.length();
         ArrayList matchedIndex=new ArrayList();
@@ -25,7 +20,7 @@ import java.util.ArrayList;
             }
         }
         System.out.println(matchedIndex);
+        return matchedIndex;
     }
-    public static ArrayList(String text, String pattern){
-    }
+    
     }
